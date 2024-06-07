6abfe632d10e630ba2a19e7cee4893cda53886a9
Cudv <cudv@bsr.com.vn>
2024-06-07 22:15:14 +0700

    update welcome text

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -9,23 +9,18 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
         <h1 className="text-lg font-semibold">
-          Welcome to the hoctienganh-ai-chatbot
+          Chào mừng bạn
         </h1>
         <p className="leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template with baked in model observability built with:{' '}<br></br>
-          <ExternalLink href="https://segment.com/customer-data-platform/">Twilio Segment</ExternalLink>, {' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>, the{' '}
-          <ExternalLink href="https://sdk.vercel.ai">
-            Vercel AI SDK
-          </ExternalLink>
-          , and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
+          Chúng tôi rất vui mừng chào đón bạn đến với ứng dụng học Anh văn nâng cao khả năng giao tiếp. Dưới đây là một số hướng dẫn để bạn bắt đầu sử dụng ứng dụng:{' '}<br></br>
         </p>
         <p className="leading-normal text-muted-foreground">
-        `You can ask me things like:<br></br> &ldquo;What&rsquo;s the price of a certain stock?&rdquo;<br></br> &ldquo;What are five great stocks?&rdquo;<br></br>&ldquo;Buy 300 stocks&rdquo;`
+          Ví Dụ Nhận Dạng Giọng Nói:<br></br>
+          Nhấn nút có biểu tượng Microphone bên dưới và Nói: &quot;Hot news in Vietnam?&quot;. Nhấn lần nữa để tắt Mic.<br></br>
+          
+          Ví Dụ Tương Tác Văn Bản:<br></br>
+          Nhập: &quot;Luyện ngữ pháp Thì Hiện tại hoàn thành.&quot;<br></br>
+          
         </p>
       </div>
     </div>
