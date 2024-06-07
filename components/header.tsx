393d6192064cd6b89ed85963bb04a388894756c1
Cudv <cudv@bsr.com.vn>
2024-06-07 09:28:13 +0700

    fix github link

--- components/header.tsx
+++ components/header.tsx
@@ -59,7 +59,7 @@ export function Header() {
       <div className="hidden flex items-center justify-end space-x-2">
         <a
           target="_blank"
-          href="https://github.com/vaithschmitz/segment-ai-copilot"
+          href="https://github.com/Cudv/hoctienganh-ai-chatbot"
           rel="noopener noreferrer"
           className={cn(buttonVariants({ variant: 'outline' }))}
         >
