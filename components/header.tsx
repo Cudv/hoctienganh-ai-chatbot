a0f4cfad9c89e4bc3b5503014cda9f8c1833b072
Cudv <cudv@bsr.com.vn>
2024-06-07 09:27:18 +0700

    hide Github button

--- components/header.tsx
+++ components/header.tsx
@@ -56,7 +56,7 @@ export function Header() {
           <UserOrLogin />
         </React.Suspense>
       </div>
-      <div className="flex items-center justify-end space-x-2">
+      <div className="hidden flex items-center justify-end space-x-2">
         <a
           target="_blank"
           href="https://github.com/vaithschmitz/segment-ai-copilot"
